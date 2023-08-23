da5e72745fc26c94338874e082c425adfaf0d613
Linda Brandão <linda.15brandao@gmail.com>
2023-08-23 11:38:27 -0400

    site da alura-tracker

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ nomeTarefa.descricao || "Tarefa sem descrição"}}
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="nomeTarefa.duracaoEmSegundos"></Cronometro>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import Box from './Box.vue';
+import ITarefa from '@/interfaces/ITarefa';
+
+export default defineComponent({
+    name: "TarefaVue",
+    props: {
+        nomeTarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    },
+    components: {
+        Cronometro,
+        Box
+    }
+})
+</script>
